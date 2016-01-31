d33284aa130c788dbaa0176162af8a9ddba87280
amolpatil <amol.patil8187@gmail.com>
2016-01-31 02:35:30 -0500

    First check-in

---
+++ src/main/java/com/health/FitnessApplication.java
@@ -0,0 +1,12 @@
+package com.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessApplication.class, args);
+	}
+}
