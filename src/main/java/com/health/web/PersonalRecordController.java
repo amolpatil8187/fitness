917ce9252df4f9e63d74656b0a99cfb75331f4a7
amolpatil <amol.patil8187@gmail.com>
2016-02-12 00:17:31 -0500

    Added web support

--- src/main/java/com/health/web/PersonalRecordController.java
+++ src/main/java/com/health/web/PersonalRecordController.java
@@ -1,17 +1,21 @@
 package com.health.web;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Method;
 
 /**
  * Created by amolpatil on 1/31/16.
  */
 @RestController
-@RequestMapping("/")
+@RequestMapping("/personal-records")
 public class PersonalRecordController {
 
-    @RequestMapping("/amanda")
-    public String get(){
-        return "Mon Amour!";
+    @RequestMapping(method = RequestMethod.GET, value = "{id}")
+    public String get(@PathVariable String id){
+        return id;
     }
 }
